5a84da9f31ace6473c3d1a8afe5a0cd29ea277bd
dannylin0711 <dannylin0711@gmail.com>
2021-03-04 02:58:19 +0800

    Update #2

--- assets/js/model.js
+++ assets/js/model.js
@@ -1,15 +1,15 @@
-
 const live2d = PIXI.live2d;
 class Live2dModel {
     constructor() {
-        this.app = new PIXI.Application({width:1280,
-            height:720,
+        this.app = new PIXI.Application({
+            width: 1280,
+            height: 720,
             // resolution: window.devicePixelRatio || 1,
             transparent: true,
             autoStart: true,
             resizeTo: window,
             antialias: true,
-         });
+        });
         this.model;
         this.pos_x = 0.0;
         this.pos_y = 0.0;
@@ -20,7 +20,7 @@ class Live2dModel {
         this.param_eye_ball_x = 0;
         this.param_eye_ball_y = 0;
         this.moc;
-        this.modelSize = 50;
+        this.modelSize = 1;
         this.modelX = 50;
         this.modelY = 50;
         this.interval;
@@ -60,68 +60,67 @@ class Live2dModel {
 
     // }
     destroy() {
-        this.app.destroy();
-        this.model = null;
+            this.app.destroy();
+            this.model = null;
+        }
+        // updateParameter() {
+        //     this.emptyAnimation.evaluate = (time, weight, blend, target) => {
+        //         if (this.param_angle_x >= 0) {
+        //             target.parameters.values[this.param_angle_x] = blend(
+        //                 target.parameters.values[this.param_angle_x],
+        //                 this.pos_x * 30,
+        //                 0,
+        //                 weight
+        //             );
+        //         }
+        //         if (this.param_angle_y >= 0) {
+        //             target.parameters.values[this.param_angle_y] = blend(
+        //                 target.parameters.values[this.param_angle_y], -this.pos_y * 30,
+        //                 0,
+        //                 weight
+        //             );
+        //         }
+        //         if (this.param_body_angle_x >= 0) {
+        //             target.parameters.values[this.param_body_angle_x] = blend(
+        //                 target.parameters.values[this.param_body_angle_x],
+        //                 this.pos_x * 10,
+        //                 0,
+        //                 weight
+        //             );
+        //         }
+        //         if (this.param_eye_ball_x >= 0) {
+        //             target.parameters.values[this.param_eye_ball_x] = blend(
+        //                 target.parameters.values[this.param_eye_ball_x],
+        //                 this.pos_x,
+        //                 0,
+        //                 weight
+        //             );
+        //         }
+        //         if (this.param_eye_ball_y >= 0) {
+        //             target.parameters.values[this.param_eye_ball_y] = blend(
+        //                 target.parameters.values[this.param_eye_ball_y], -this.pos_y,
+        //                 0,
+        //                 weight
+        //             );
+        //         }
+        //     };
+        //     this.model.animator.getLayer("Drag").play(this.emptyAnimation);
+        // }
+    onResize(event) {
+        if (event === void 0) {
+            event = null;
+        }
+        let width = window.innerWidth;
+        let height = (width / 16.0) * 9.0;
+        // this.app.view.style.width = width + "px";
+        // this.app.view.style.height = height + "px";
+        // this.app.renderer.resize(width, height);
+        // this.model.position = new PIXI.Point(width * this.modelX / 100, height * this.modelY / 100);
+        console.log([this.model.x, this.model.y]);
+        this.model.scale.set(width * this.modelSize * 1.2 / 10000);
+        console.log(width * this.modelSize * 1.2 / 10000);
+        // if (this.model.masks) this.model.masks.resize(this.app.view.width, this.app.view.height);
     }
-    // updateParameter() {
-    //     this.emptyAnimation.evaluate = (time, weight, blend, target) => {
-    //         if (this.param_angle_x >= 0) {
-    //             target.parameters.values[this.param_angle_x] = blend(
-    //                 target.parameters.values[this.param_angle_x],
-    //                 this.pos_x * 30,
-    //                 0,
-    //                 weight
-    //             );
-    //         }
-    //         if (this.param_angle_y >= 0) {
-    //             target.parameters.values[this.param_angle_y] = blend(
-    //                 target.parameters.values[this.param_angle_y], -this.pos_y * 30,
-    //                 0,
-    //                 weight
-    //             );
-    //         }
-    //         if (this.param_body_angle_x >= 0) {
-    //             target.parameters.values[this.param_body_angle_x] = blend(
-    //                 target.parameters.values[this.param_body_angle_x],
-    //                 this.pos_x * 10,
-    //                 0,
-    //                 weight
-    //             );
-    //         }
-    //         if (this.param_eye_ball_x >= 0) {
-    //             target.parameters.values[this.param_eye_ball_x] = blend(
-    //                 target.parameters.values[this.param_eye_ball_x],
-    //                 this.pos_x,
-    //                 0,
-    //                 weight
-    //             );
-    //         }
-    //         if (this.param_eye_ball_y >= 0) {
-    //             target.parameters.values[this.param_eye_ball_y] = blend(
-    //                 target.parameters.values[this.param_eye_ball_y], -this.pos_y,
-    //                 0,
-    //                 weight
-    //             );
-    //         }
-    //     };
-    //     this.model.animator.getLayer("Drag").play(this.emptyAnimation);
-    // }
-    // onResize(event) {
-    //     if (event === void 0) {
-    //         event = null;
-    //     }
-    //     let width = window.innerWidth;
-    //     let height = (width / 16.0) * 9.0;
-    //     this.app.view.style.width = width + "px";
-    //     this.app.view.style.height = height + "px";
-    //     this.app.renderer.resize(width, height);
-    //     this.model.position = new PIXI.Point(width * this.modelX / 100, height * this.modelY / 100);
-    //     this.model.scale = new PIXI.Point(
-    //         width * this.modelSize / 100,
-    //         width * this.modelSize / 100
-    //     );
-    //     if (this.model.masks) this.model.masks.resize(this.app.view.width, this.app.view.height);
-    // }
 
 
     // hitmotion(){
@@ -130,29 +129,48 @@ class Live2dModel {
 
 
     async loadModel(modelName) {
+        this.destroy();
+        this.app = new PIXI.Application({
+            width: 1280,
+            height: 720,
+            // resolution: window.devicePixelRatio || 1,
+            transparent: true,
+            autoStart: true,
+            resizeTo: window,
+            antialias: true,
+        });
         const path = './assets/live2d/';
         const folderPath = path + modelName + '/';
         if (modelName === 'grace_yukata_ver5') modelName = 'grace_yukata';
         let jsonPath = folderPath + (modelName === 'grace_ver5' ? modelName + '_0102' : modelName === 'rasis_ver5' ? modelName + '_0103' : modelName === "natsuhi_otona" ? modelName + '_0100' : modelName + '_0101') + '.model3.json';
-            //let modelData = await fetch(jsonPath).then(response => response.json())
+        //let modelData = await fetch(jsonPath).then(response => response.json())
         // let modelData = await $.ajax({ url: jsonPath, dataType: 'json' });
         // modelData = modelData.FileReferences;
         // console.log(modelData);
         console.log(jsonPath);
-        let modelData = await live2d.Live2DModel.from(jsonPath);
+        let modelData = await live2d.Live2DModel.from(jsonPath, { idleMotionGroup: 'Idle', motionPreload: "ALL" });
+        this.model = modelData;
         console.log(modelData);
         document.getElementById('live2d').appendChild(this.app.view);
         this.app.stage.addChild(modelData);
         // this.app.stage.addChild(modelData.mask);
-        modelData.scale.set(0.25);
-        modelData.motion('In',0,1);
+        //modelData.scale.set(0.25);
+        await modelData.motion('In', 0, 1);
         modelData.buttonMode = true;
-        modelData.on("click",(e)=>{
-            this.model.motion("Ok",0,3);
+        modelData.on("click", (e) => {
+            this.model.motion("Ok", 0, 2);
         });
+        this.model.anchor.set(0.5, 0.5);
 
+
+        this.model.x = window.innerWidth / 2;
+        this.model.y = (this.model.x / 16.0) * 9.0;
+        live2d.config.motionFadingDuration = 0;
+        live2d.config.idleMotionFadingDuration = 0;
+        live2d.config.expressionFadingDuration = 0;
+        live2d.config.logLevel = 0;
         this.model = modelData;
-        
+        this.onResize("");
         // this.app.stage.addChild(modelData);
         // this.app.stage.addChild(this.model);
         // this.model.scale.set(1.0);
--- assets/js/wallpaper.js
+++ assets/js/wallpaper.js
@@ -1,4 +1,4 @@
-let modelName = "konoha_ver5";
+let modelName = "left_ver5";
 let model = new Live2dModel();
 model.loadModel(modelName);
 // window.onresize = model.onResize();
@@ -6,47 +6,46 @@ const bgVideo = document.getElementById("bg-video");
 const bg = document.getElementById('bg');
 const bgm = document.getElementById('bgm');
 window.wallpaperPropertyListener = {
-  applyUserProperties(properties) {
-    if (properties.modelName) {
-      let modelName = properties.modelName.value;
-      model.loadModel(modelName);
-    }
-    if (properties.modelX) {
-      let modelX = properties.modelX.value;
-      model.modelX = modelX;
-    }
-    if (properties.modelY) {
-      let  modelY = properties.modelY.value;
-      model.modelY = modelY;
-    }
-    if (properties.modelSize) {
-      let modelSize = properties.modelSize.value;
-      model.modelSize = modelSize;
-    }
-    
-    // model.onResize();
+    applyUserProperties(properties) {
+        if (properties.modelName) {
+            let modelName = properties.modelName.value;
+            model.loadModel(modelName);
+        }
+        if (properties.modelX) {
+            let modelX = properties.modelX.value;
+            model.modelX = modelX;
+        }
+        if (properties.modelY) {
+            let modelY = properties.modelY.value;
+            model.modelY = modelY;
+        }
+        if (properties.modelSize) {
+            let modelSize = properties.modelSize.value;
+            model.modelSize = modelSize;
+        }
 
-    // other
-    if (properties.bgBright) {
-      bg.style.filter = `brightness(${properties
+        model.onResize();
+
+        // other
+        if (properties.bgBright) {
+            bg.style.filter = `brightness(${properties
         .bgBright.value / 100})`;
+        }
+        if (properties.bgType) {
+            if (properties.bgType.value === 'video') {
+                bgVideo.style.display = 'inline';
+            } else {
+                bgVideo.style.display = 'none';
+            }
+        }
+        if (properties.volume) {
+            bgm.volume = properties.volume.value / 100;
+        }
     }
-    if (properties.bgType) {
-      if(properties.bgType.value === 'video') {
-        bgVideo.style.display = 'inline';
-      }
-      else {
-        bgVideo.style.display = 'none';
-      }
-    }
-    if (properties.volume) {
-      bgm.volume = properties.volume.value / 100;
-    }
-  }
 };
 
 
-var onMouseDownFlag = 0;
+// var onMouseDownFlag = 0;
 
 // addEventListener('dblclick',function(e){
 //   console.log(e);
