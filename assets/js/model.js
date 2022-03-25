8efef4bc3a43d873ee70f340c4eee32a6f7e603e
dannylin0711 <dannylin0711@gmail.com>
2022-03-25 13:05:07 +0800

    Update 0214
    Update Two model support
    Added CRT Effect
    
    TODO: Glass Break Effect

--- assets/js/model.js
+++ assets/js/model.js
@@ -1,4 +1,6 @@
 const live2d = PIXI.live2d;
+const width = window.innerWidth;
+const height = window.innerHeight;
 class Live2dModel {
     constructor() {
         this.app = new PIXI.Application({
@@ -10,7 +12,9 @@ class Live2dModel {
             resizeTo: window,
             antialias: true,
         });
+        this.mainContainer;
         this.model;
+        this.model2;
         this.pos_x = 0.0;
         this.pos_y = 0.0;
         this.emptyAnimation;
@@ -26,15 +30,36 @@ class Live2dModel {
         this.interval;
         this.times = 12;
         this.originalHeight;
+        this.twomodels = false;
+        this.needResetMotionFade = false;
+        this.playingMotion = false;
+        this.crtEffect = false;
+        this.glassBreakEffect = false;
+        this.useVideo = true;
+        this.videoURL = "assets/background/bg.webm";
+        this.videoContext;
+        this.bgURL = "assets/background/bg.jpg";
+        this.bgContext;
     }
 
     destroy() {
 
-        this.app.destroy({
-            removeView: true,
-            stageOptions: true,
-        });
-        this.model = null;
+        //this.app.destroy({
+        //    removeView: true,
+        //    stageOptions: true,
+        //});
+        if (!(typeof this.model === 'undefined')) {
+            this.model.destroy();
+        }
+
+        if (!(typeof this.model2 === 'undefined')) {
+            this.model2.destroy();
+        }
+
+
+        this.model = undefined;
+        this.model2 = undefined;
+        this.twomodels = false;
     }
 
     onResize(event) {
@@ -42,48 +67,291 @@ class Live2dModel {
             event = null;
         }
         console.log(event);
-        let width = window.innerWidth;
-        let height = window.innerHeight;
+
 
         let scale = height / this.originalHeight;
-        console.log([height, this.model.height, this.originalHeight]);
+        //console.log([height, this.model.height, this.originalHeight]);
 
-        this.model.x = width / 2 + this.modelX * 9;
-        this.model.y = height / 2 + this.modelY * 3;
+        this.model.x = width / 2 + this.modelX * width / 100;
+        this.model.y = height / 2 + this.modelY * height / 100;
         console.log([this.model.x, this.model.y]);
-        this.model.scale.set(scale * (this.modelSize / 50));
+        this.model.scale.set(scale * (this.modelSize / 30));
         console.log(scale * this.modelSize);
-        if (this.model.masks) this.model.masks.resize(this.app.view.width, this.app.view.height);
+        if (this.twomodels) {
+            this.model2.x = width / 2 + this.modelX * width / 100;
+            this.model2.y = height / 2 + this.modelY * height / 100;
+            console.log([this.model2.x, this.model2.y]);
+            this.model2.scale.set(scale * (this.modelSize / 30));
+            console.log(scale * this.modelSize);
+        }
+        //if (this.model.masks) this.model.masks.resize(this.app.view.width, this.app.view.height);
     }
+
+
 
 
     async loadModel(modelName) {
         this.destroy();
+        this.app.stage.sortableChildren = true;
+        this.mainContainer = new PIXI.Container();
+        //this.app = new PIXI.Application({
+        //    width: 1280,
+        //    height: 720,
+        //    resolution: window.devicePixelRatio || 1,
+        //    transparent: true,
+        //    autoStart: true,
+        //    resizeTo: window,
+        //    antialias: true,
+        //});
+        var videoTexture = PIXI.Texture.from(this.videoURL);
+        videoTexture.baseTexture.resource.source.loop = true;
+        this.videoContext = new PIXI.Sprite(videoTexture);
+        this.videoContext.anchor.set(0.5, 0.5)
+        this.videoContext.x = width / 2;
+        this.videoContext.y = height / 2;
+        this.videoContext.preload = 'auto'
+        this.videoContext.autoplay = true;
+        this.videoContext.zIndex = -1
+        this.videoContext.scale.set(width / 1920, height / 1080);
 
-        this.app = new PIXI.Application({
-            width: 1280,
-            height: 720,
-            resolution: window.devicePixelRatio || 1,
-            transparent: true,
-            autoStart: true,
-            resizeTo: window,
-            antialias: true,
-        });
+        if (this.useVideo) {
+            this.app.stage.addChild(this.videoContext);
+        }
+
+        var bgTexture = PIXI.Texture.from(this.bgURL);
+        this.bgContext = new PIXI.Sprite(bgTexture);
+        this.bgContext.anchor.set(0.5, 0.5)
+        this.bgContext.x = width / 2;
+        this.bgContext.y = height / 2;
+        this.bgContext.zIndex = -2
+
+
+        this.app.stage.addChild(this.bgContext);
+
+
+
+
 
         const path = './assets/live2d/';
         const folderPath = path + modelName + '/';
+        const mion = 'mion_xhrono';
+        const jsonPathMion = path + mion + '/' + mion + '_0101.model3.json'
+
+        if (modelName === 'rasis_xhrono') this.twomodels = true;
         if (modelName === 'grace_yukata_ver5') modelName = 'grace_yukata';
         let jsonPath = folderPath + (modelName === 'grace_ver5' ? modelName + '_0102' : modelName === 'rasis_ver5' ? modelName + '_0103' : modelName === "natsuhi_otona" ? modelName + '_0100' : modelName + '_0101') + '.model3.json';
-        let modelData = await live2d.Live2DModel.from(jsonPath, { idleMotionGroup: 'Idle', motionPreload: "ALL", autoInteract: false });
-        this.model = modelData;
-        document.getElementById('live2d').appendChild(this.app.view);
-        this.app.stage.addChild(this.model);
+        //let jsonPath2 = folderPath + mion + '_0101.model3.json'
+        console.log(jsonPath, jsonPathMion);
+        let modelSecondData = await live2d.Live2DModel.from(jsonPathMion, { motionPreload: "ALL", autoInteract: false });
+        let modelData = await live2d.Live2DModel.from(jsonPath, { motionPreload: "ALL", autoInteract: false });
 
-        await this.model.motion('In', 0, 2);
+
+
+        //modelData.once('load',()=>{
+        this.model = modelData;
+        this.model.motion('In', 0, 2);
         this.model.anchor.set(0.5, 0.5);
+        this.model.zIndex = 1;
+        this.app.stage.addChild(this.model);
         this.originalHeight = this.model.height;
-        live2d.config.logLevel = 0;
-        this.onResize("");
+        //this.onResize(1);
+        //})
+
+        //modelSecondData.once('load',()=>{
+        if (this.twomodels) {
+            this.model2 = modelSecondData;
+            this.model2.motion('In', 0, 2);
+            this.model2.anchor.set(0.5, 0.5);
+            this.model2.zIndex = 2;
+            this.app.stage.addChild(this.model2);
+
+        }
+        //})
+        this.model.internalModel.motionManager.motionGroups.Idle[0]._fadeInSeconds = 0.015
+        if (this.twomodels) this.model2.internalModel.motionManager.motionGroups.Idle[0]._fadeInSeconds = 0.015
+
+
+
+
+
+        let okButton = new PIXI.Graphics();
+        okButton.beginFill(0xffffff);
+        okButton.drawCircle(0, 0, 30);
+        okButton.endFill();
+        let okText = new PIXI.Text('OK', { fontFamily: 'Arial', fontSize: 24, fill: 0x1010ff, align: 'center' });
+        okText.anchor.set(0.5, 0.5);
+        okButton.addChild(okText);
+        okButton.position.set(35, 35);
+        okButton.on('pointerup', () => {
+            this.model.motion("Ok", 0, 3);
+            if (this.twomodels) this.model2.motion("Ok", 0, 3);
+        });
+        okButton.zIndex = 5;
+        okButton.interactive = true;
+        okButton.buttonMode = true;
+        this.app.stage.addChild(okButton);
+
+        let goodButton = new PIXI.Graphics();
+        goodButton.beginFill(0xffffff);
+        goodButton.drawCircle(0, 0, 30);
+        goodButton.endFill();
+        let goodText = new PIXI.Text('Good', { fontFamily: 'Arial', fontSize: 20, fill: 0x1010ff, align: 'center' });
+        goodText.anchor.set(0.5, 0.5);
+        goodButton.addChild(goodText);
+        goodButton.position.set(100, 35);
+        goodButton.on('pointerup', () => {
+            this.needResetMotionFade = true;
+            this.model.motion("R_Good", 0, 3);
+            this.model.internalModel.motionManager.motionGroups.Idle[0]._fadeInSeconds = 1.5
+            if (this.twomodels) {
+                this.model2.motion("R_Good", 0, 3);
+                this.model2.internalModel.motionManager.motionGroups.Idle[0]._fadeInSeconds = 1.5
+            }
+            if (!this.playingMotion) {
+                this.playingMotion = true;
+                setTimeout(() => {
+                    this.model.internalModel.motionManager.motionGroups.Idle[0]._fadeInSeconds = 0.015
+                    if (this.twomodels) this.model2.internalModel.motionManager.motionGroups.Idle[0]._fadeInSeconds = 0.015
+                    this.playingMotion = false;
+                }, this.model.internalModel.motionManager.motionGroups.R_Good[0]._loopDurationSeconds * 1000 + 1500)
+            }
+        });
+        goodButton.zIndex = 5;
+        goodButton.interactive = true;
+        goodButton.buttonMode = true;
+        this.app.stage.addChild(goodButton);
+
+        let badButton = new PIXI.Graphics();
+        badButton.beginFill(0xffffff);
+        badButton.drawCircle(0, 0, 30);
+        badButton.endFill();
+        let badText = new PIXI.Text('Bad', { fontFamily: 'Arial', fontSize: 20, fill: 0x1010ff, align: 'center' });
+        badText.anchor.set(0.5, 0.5);
+        badButton.addChild(badText);
+        badButton.position.set(165, 35);
+        badButton.on('pointerup', () => {
+            this.model.motion("R_Bad", 0, 3);
+            this.model.internalModel.motionManager.motionGroups.Idle[0]._fadeInSeconds = 1.5
+            if (this.twomodels) {
+                this.model2.motion("R_Bad", 0, 3);
+                this.model2.internalModel.motionManager.motionGroups.Idle[0]._fadeInSeconds = 1.5
+            }
+            if (!this.playingMotion) {
+                this.playingMotion = true;
+                setTimeout(() => {
+                    this.model.internalModel.motionManager.motionGroups.Idle[0]._fadeInSeconds = 0.015
+                    if (this.twomodels) this.model2.internalModel.motionManager.motionGroups.Idle[0]._fadeInSeconds = 0.015
+                    this.playingMotion = false;
+                }, this.model.internalModel.motionManager.motionGroups.R_Bad[0]._loopDurationSeconds * 1000 + 1500)
+            }
+        });
+        badButton.zIndex = 5;
+        badButton.interactive = true;
+        badButton.buttonMode = true;
+        this.app.stage.addChild(badButton);
+
+        let vGoodButton = new PIXI.Graphics();
+        vGoodButton.beginFill(0xffffff);
+        vGoodButton.drawCircle(0, 0, 30);
+        vGoodButton.endFill();
+        let vGoodText = new PIXI.Text('VGood', { fontFamily: 'Arial', fontSize: 18, fill: 0x1010ff, align: 'center' });
+        vGoodText.anchor.set(0.5, 0.5);
+        vGoodButton.addChild(vGoodText);
+        vGoodButton.position.set(230, 35);
+        vGoodButton.on('pointerup', () => {
+
+            this.model.motion("R_Verygood", 0, 3);
+            this.model.internalModel.motionManager.motionGroups.Idle[0]._fadeInSeconds = 1.5
+            if (this.twomodels) {
+                this.model2.motion("R_Verygood", 0, 3);
+                this.model2.internalModel.motionManager.motionGroups.Idle[0]._fadeInSeconds = 1.5
+
+            }
+            if (!this.playingMotion) {
+                this.playingMotion = true;
+                setTimeout(() => {
+                    this.model.internalModel.motionManager.motionGroups.Idle[0]._fadeInSeconds = 0.015
+                    if (this.twomodels) this.model2.internalModel.motionManager.motionGroups.Idle[0]._fadeInSeconds = 0.015
+                    this.playingMotion = false;
+                }, this.model.internalModel.motionManager.motionGroups.R_Verygood[0]._loopDurationSeconds * 1000 + 1500)
+            }
+        });
+        vGoodButton.zIndex = 5;
+        vGoodButton.interactive = true;
+        vGoodButton.buttonMode = true;
+        this.app.stage.addChild(vGoodButton);
+
+
+        if (this.crtEffect) {
+            console.log("Applying CRT Effect")
+            var crtEffect = new PIXI.filters.CRTFilter()
+            crtEffect.padding = width
+
+            var adjustEffect = new PIXI.filters.AdjustmentFilter()
+            adjustEffect.saturation = 0.5
+
+            var rgbSplitEffect = new PIXI.filters.RGBSplitFilter()
+            rgbSplitEffect.red.x = -5
+            rgbSplitEffect.red.y = 0
+            rgbSplitEffect.green.x = 0
+            rgbSplitEffect.green.y = 0
+            rgbSplitEffect.blue.x = 5
+            rgbSplitEffect.blue.y = 0
+            this.app.stage.filters = [crtEffect, adjustEffect, rgbSplitEffect]
+        } else {
+            this.app.stage.filters = undefined
+        }
+
+        if (this.glassBreakEffect) {
+
+        }
+
+
+        this.onResize(2);
+        document.getElementById('live2d').appendChild(this.app.view);
+    }
+
+    updateCRTEffect() {
+        if (this.crtEffect) {
+            var crtEffect = new PIXI.filters.CRTFilter()
+            crtEffect.padding = width
+
+            var adjustEffect = new PIXI.filters.AdjustmentFilter()
+            adjustEffect.saturation = 0.5
+
+            var rgbSplitEffect = new PIXI.filters.RGBSplitFilter()
+            rgbSplitEffect.red.x = -5
+            rgbSplitEffect.red.y = 0
+            rgbSplitEffect.green.x = 0
+            rgbSplitEffect.green.y = 0
+            rgbSplitEffect.blue.x = 5
+            rgbSplitEffect.blue.y = 0
+            this.app.stage.filters = [crtEffect, adjustEffect, rgbSplitEffect]
+        } else {
+            this.app.stage.filters = []
+        }
+    }
+
+    updateBg(key) {
+
+        if (!(typeof this.videoContext === "undefined")) {
+            this.app.stage.removeChild(this.videoContext)
+            var videoTexture = PIXI.Texture.from(this.videoURL);
+            videoTexture.baseTexture.resource.source.loop = true;
+            this.videoContext = new PIXI.Sprite(videoTexture);
+            this.videoContext.anchor.set(0.5, 0.5)
+            this.videoContext.x = width / 2;
+            this.videoContext.y = height / 2;
+            this.videoContext.preload = 'auto'
+            this.videoContext.autoplay = true;
+            this.videoContext.zIndex = -1
+            this.videoContext.scale.set(width / 1920, height / 1080);
+            if (this.useVideo) {
+                this.app.stage.addChild(this.videoContext)
+            } else {
+                this.app.stage.removeChild(this.videoContext)
+            }
+        }
 
     }
 }
--- assets/js/wallpaper.js
+++ assets/js/wallpaper.js
@@ -14,11 +14,15 @@ window.wallpaperPropertyListener = {
             if (!(typeof model.model === "undefined"))
                 if (typeof model.model.motion === "function") {
                     model.model.motion("Out", 0, 3);
+                    if (model.twomodels) {
+                        model.model2.motion("Out", 0, 3);
+                    }
                     oldModelname = modelName;
+                    console.log("???");
                 }
 
             modelName = properties.modelName.value;
-            switch (oldModelname) { // need to set model out motion time if the model reappear after out motion
+            switch (oldModelname) {
                 case "rasis_ver5":
                     setTimeout(() => { model.loadModel(modelName) }, 1583);
                     break;
@@ -42,6 +46,15 @@ window.wallpaperPropertyListener = {
                 case "nianoa_ver6":
                     setTimeout(() => { model.loadModel(modelName) }, 1717);
                     break;
+                case "suzukihina_ver6":
+                    setTimeout(() => { model.loadModel(modelName) }, 1600);
+                    break;
+                case "tanakahime_ver6":
+                    setTimeout(() => { model.loadModel(modelName) }, 1550);
+                    break;
+                case "rasis_xhrono":
+                    setTimeout(() => { model.loadModel(modelName) }, 2183);
+                    break;
                 default:
                     setTimeout(() => { model.loadModel(modelName) }, 1800);
                     break;
@@ -62,30 +75,49 @@ window.wallpaperPropertyListener = {
 
         // other
         if (properties.bgBright) {
-            bg.style.filter = `brightness(${properties
-        .bgBright.value / 100})`;
+            // bg.style.filter = `brightness(${properties.bgBright.value / 100})`;
         }
         if (properties.bgType) {
             if (properties.bgType.value === 'video') {
-                bgVideo.style.display = 'inline';
+                //bgVideo.style.display = 'inline';
+                model.useVideo = true
+                model.updateBg(1)
             } else {
-                bgVideo.style.display = 'none';
+                //bgVideo.style.display = 'none';
+                model.useVideo = false
+                model.updateBg(1)
             }
         }
 
         if (properties.backgroundversion) {
             if (properties.backgroundversion.value === 'bg') {
                 bg.style.backgroundImage = 'url(./assets/background/bg.jpg)';
-                bgVideo.src = './assets/background/bg.webm';
+                //bgVideo.src = './assets/background/bg.webm';
+                model.bgURL = "assets/background/bg.jpg"
+                model.videoURL = "assets/background/bg.webm";
+                model.updateBg(2);
             } else {
                 bg.style.backgroundImage = 'url(./assets/background/bg2.png)';
-                bgVideo.src = './assets/background/bg2.webm';
+                //bgVideo.src = './assets/background/bg2.webm';
+                model.bgURL = "assets/background/bg2.jpg"
+                model.videoURL = "assets/background/bg2.webm";
+                model.updateBg(2);
             }
         }
 
-        //BGM volume fix for new version Wallpaper Engine
         if (properties.bgm) {
             bgm.volume = properties.bgm.value / 100;
+        }
+
+        if (properties.crteffect) {
+            console.log(properties.crteffect.value)
+            model.crtEffect = properties.crteffect.value
+            model.updateCRTEffect()
+        }
+
+        if (properties.glassbreakeffect) {
+            console.log(properties.glassbreakeffect.value)
+            model.glassBreakEffect = properties.glassbreakeffect.value
         }
         model.onResize("t");
     }
@@ -93,13 +125,21 @@ window.wallpaperPropertyListener = {
 
 document.getElementById('live2d').addEventListener('pointerdown', (e) => {
     model.model.focus(e.clientX, e.clientY);
-    model.model.motion("Ok", 0, 3);
+    if (model.twomodels) {
+        model.model2.focus(e.clientX, e.clientY);
+        //model.model2.motion("Ok",0,3);
+    }
+    //model.model.motion("Ok", 0, 3);
     flag = true;
 })
+
 
 document.getElementById('live2d').addEventListener('pointermove', (e) => {
     if (flag) {
         model.model.focus(e.clientX, e.clientY);
+        if (model.twomodels) {
+            model.model2.focus(e.clientX, e.clientY);
+        }
     }
 });
 
@@ -108,36 +148,46 @@ document.getElementById('live2d').addEventListener('pointerup', (e) => {
     let width = window.innerWidth;
     let height = window.innerHeight;
     model.model.focus(width / 2 + model.modelX * 9, height / 2 + model.modelY * 3);
+    if (model.twomodels) {
+        model.model2.focus(width / 2 + model.modelX * 9, height / 2 + model.modelY * 3);
+    }
 })
 
-// Still figuring out how to handle/change motion
+window.addEventListener('onclick', (e) => {
+    model.model.motion("Ok", 0, 2);
+});
 
-// window.addEventListener('onclick', (e) => {
-//     model.model.motion("Ok", 0, 2);
-// });
+window.addEventListener('keydown', (e) => {
+    console.log(e.key);
+    if (e.isComposing || e.key === 'o') {
+        model.model.motion("Ok", 0, 2);
+        return;
+    }
 
-// window.addEventListener('keydown', (e) => {
-//     console.log(e.key);
-//     if (e.isComposing || e.key === 'o') {
-//         model.model.motion("Ok", 0, 2);
-//         return;
-//     }
+    if (e.isComposing || e.key === 'g') {
+        model.model.motion("R_Good", 0, 2);
+        if (model.twomodels) {
+            model.model2.motion("R_Good", 0, 2);
+        }
+        return;
+    }
 
-//     if (e.isComposing || e.key === 'g') {
-//         model.model.motion("R_Good", 0, 2);
-//         return;
-//     }
+    if (e.isComposing || e.key === 'v') {
+        model.model.motion("R_Verygood", 0, 2);
+        if (model.twomodels) {
+            model.model2.motion("R_Verygood", 0, 2);
+        }
+        return;
+    }
 
-//     if (e.isComposing || e.key === 'v') {
-//         model.model.motion("R_Verygood", 0, 2);
-//         return;
-//     }
-
-//     if (e.isComposing || e.key === 'b') {
-//         model.model.motion("R_Bad", 0, 2);
-//         return;
-//     }
-// })
+    if (e.isComposing || e.key === 'b') {
+        model.model.motion("R_Bad", 0, 2);
+        if (model.twomodels) {
+            model.model2.motion("R_Bad", 0, 2);
+        }
+        return;
+    }
+})
 
 window.addEventListener('resize', (e) => {
     model.onResize("");
